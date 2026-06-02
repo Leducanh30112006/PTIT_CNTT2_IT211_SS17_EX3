234353a89cc092db4a6b4a655f877748e7d9f36d
Leducanh30112006 <le1duc2anh3@gmail.com>
2026-06-02 23:13:07 +0700

    btvn

--- src/main/java/com/ra/ptit_cntt2_it211_ss17_ex3/ProjectManagementController.java
+++ src/main/java/com/ra/ptit_cntt2_it211_ss17_ex3/ProjectManagementController.java
@@ -11,25 +11,32 @@ public class ProjectManagementController {
 
     @Autowired
     private TaskService taskService;
+
     @PostMapping("/projects")
     @PreAuthorize("hasRole('PO')")
     public String createProject() {
+
         return "Dự án mới đã được tạo bởi Product Owner.";
     }
+
     @PutMapping("/sprints/{id}")
     @PreAuthorize("hasRole('SM')")
     public String manageSprint(@PathVariable UUID id) {
+
         return "Sprint " + id + " đã được quản lý bởi Scrum Master.";
     }
+
     @PutMapping("/tasks/{taskId}/status")
     @PreAuthorize("(hasRole('DEV') or hasRole('QA')) and @taskService.isTaskOwner(#taskId, authentication.name)")
     public String updateTaskStatus(@PathVariable UUID taskId) {
-        // Logic cập nhật trạng thái task
+
         return "Trạng thái tác vụ " + taskId + " đã được cập nhật.";
     }
+
     @DeleteMapping("/tasks/{taskId}")
     @PreAuthorize("hasRole('PO') or @taskService.getTaskCreator(#taskId) == authentication.name")
     public String deleteTask(@PathVariable UUID taskId) {
+
         return "Tác vụ " + taskId + " đã được xóa.";
     }
 }
